35a084902c6bfa34121984b5fa2d4bbe0c94580d
Hernán Arica <hernanaricammm@gmail.com>
2022-03-05 00:19:14 -0300

    Se realizaron validaciones y los escuchadores de evento

--- js/main.js
+++ js/main.js
@@ -1,2 +1,29 @@
 const compose = (...functions) => data =>
-  functions.reduceRight((value, func) => func(value), data)
+	functions.reduceRight((value, func) => func(value), data);
+
+
+const $btn    = document.querySelector('.btn');
+const $fields = document.querySelectorAll('.input');
+
+const validateFields = () => {
+	let counter = 0;
+	
+	$fields.forEach($field => {
+		($field.value === '') ? $field.classList.add('is-invalid') : counter++;
+	});
+	
+	if (counter === $fields.length) {
+		console.log('Adding...');
+	}
+};
+
+$fields.forEach($field => {
+	$field.addEventListener('keypress', () => {
+		$field.classList.remove('is-invalid');
+	});
+});
+
+
+$btn.addEventListener('click', () => {
+	validateFields();
+});
